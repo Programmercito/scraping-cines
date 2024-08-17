0e3cd440e3989dc945700c7a809935cb3158044a
JOAQUIN HEREDIA <JOAQUIN.HEREDIA@LITERMI.COM>
2024-08-17 12:34:38 -0400

    Refactor ScrapingMulticine class and remove unnecessary code

--- src/main/java/org/osbo/scraping/multicine/ScrapingMulticine.java
+++ src/main/java/org/osbo/scraping/multicine/ScrapingMulticine.java
@@ -1,27 +1,13 @@
 package org.osbo.scraping.multicine;
 
-import java.nio.channels.ScatteringByteChannel;
-
-import org.osbo.scraping.model.CineRequstGetData;
-
-import kong.unirest.core.HttpResponse;
-import kong.unirest.core.JsonNode;
-import kong.unirest.core.Unirest;
-
 /**
  *
- * @author hered
+ * @author programmercito
+ *
  */
 public class ScrapingMulticine {
 
     public static void main(String[] args) {
-        String url = "https://www.multicine.com.bo/restapi/public/api/location/getdata";
-        CineRequstGetData cineRequstGetData = new CineRequstGetData();
-        cineRequstGetData.setTk(null);
-        HttpResponse<JsonNode> asJson = Unirest.post(url)
-                .body(cineRequstGetData)
-                .asJson();
-        System.out.println(asJson.getBody().toString());
-
+        System.out.println("Hello World!");
     }
 }
