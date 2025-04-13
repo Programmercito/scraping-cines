d7e4e7662a8b53d155fd925ea61f3455377bc661
programmercito <aps@outlook.com>
2025-04-13 17:08:03 -0400

    Remove unnecessary wait time and clean up Playwright test code

--- scraping/example.spec.ts
+++ scraping/example.spec.ts
@@ -1,12 +1,18 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test('scraping-multicine', async ({ page }) => {
   const url = 'https://www.multicine.com.bo/';
   await page.goto(url, { waitUntil: 'load' });
   // espero 4 segundos
-  await page.waitForTimeout(4000);
   // guardo captura de pantalla
   await page.screenshot({ path: 'multicine.png' });
 
-  
+  // consigo el compoennte visible que tenga un class
+  const element = await page.$('.locationDropdown.undefined');
+  // imprimo el contenido en html
+  const elementHtml = await element?.evaluate((el) => el.innerHTML);
+  console.log('elementHtml', elementHtml);
+
+
+
 });
