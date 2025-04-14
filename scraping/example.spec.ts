c9a87155465824bb10c86e8c42c4f0583b8b7742
programmercito <aps@outlook.com>
2025-04-13 23:23:09 -0400

    Refactor Playwright test to improve code readability by removing unnecessary whitespace and ensuring consistent function declaration style

--- scraping/example.spec.ts
+++ scraping/example.spec.ts
@@ -18,23 +18,27 @@ test('Obtener ciudades del dropdown', async ({ page }) => {
   const dropdownItems = items.locator('.dropdownItem');
   const count = await dropdownItems.count();
   console.log(`Total de elementos encontrados: ${count}`);
-  
+
   // saltandome el primer elemento voy a ir uno por uno dandole click esperando por click 3 segundos y sacando captura de cada uno de lllos 
   for (let i = 1; i < count; i++) {
     const item = dropdownItems.nth(i);
     await item.click();
     await page.waitForTimeout(3000); // espera para que cargue
     // mando a procesar la pagina a otra funcion 
-     await procesarPagina(page);
+    await procesarPagina(page);
     // captura de pantalla
     await page.screenshot({ path: `screenshot${i}.png` });
     await header.click(); // vuelvo a abrir el dropdown
   }
-  
+
 
 });
 
-function procesarPagina(page: Page) {
-  throw new Error('Function not implemented.');
+async function procesarPagina(page: Page) {
+  const list = page.locator('.grid-row.custom-row');
+  const listabotones = list.locator('.button.is-small.w-button.buy_tickets.false');
+  const count = await listabotones.count();
+  console.log(`Total de elementos encontrados: ${count}`);
+
 }
 
