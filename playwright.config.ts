1bdde41dea27c7665785a1c2c9cf42231b3debcb
programmercito <aps@outlook.com>
2025-04-15 01:04:02 -0400

    Increase test timeout to 160 seconds and add screenshot captures for dropdown city selection and movie processing.

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 60000,
+  timeout: 160000,
   testDir: './scraping',
   /* Run tests in files in parallel */
   fullyParallel: true,
