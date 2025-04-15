5fa94014f32eb0049662d94e8c703476bdc9e420
programmercito <aps@outlook.com>
2025-04-15 01:36:19 -0400

    Increase timeout in playwright.config.ts to 169990000 for improved test stability and add screenshot captures in procesarHorarios for better debugging.

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 160000,
+  timeout: 169990000,
   testDir: './scraping',
   /* Run tests in files in parallel */
   fullyParallel: true,
