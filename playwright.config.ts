88891edc58580d4197dd290aebafd5452d10da59
joa <joa123456789333@mail.com>
2025-05-25 18:00:00 -0400

    Update screenshot path in mega.spec.ts to new directory

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,9 +22,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
